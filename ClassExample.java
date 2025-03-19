8260672b40085d0ea782b55159fe649ee4b45091
Jennipher <jenniphermedina11@gmail.com>
2025-03-19 13:57:05 -0500

    first commit

---
+++ ClassExample.java
@@ -0,0 +1,14 @@
+public class Example1 {
+    public static void main(String[] args) {
+
+        //outer loop
+        for (int i = 1; i <= 5; i++) { //loops for 5 lines
+            //inner loop
+            for (int x = 1; x <= i; x++) 
+            {
+                System.out.print("*"); //prints * per line
+            }
+            System.out.println(); // move to next line
+        }
+    }
+}
